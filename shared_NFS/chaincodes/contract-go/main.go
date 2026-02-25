f20bbd555a8ba8d9935e1fc0ad7759692045d1f2
vikash-ftw <vikashbatham97@gmail.com>
2026-02-25 11:32:14 +0000

    renamed package name for main go contract directory and changes META-INF path to packaging directory

--- shared_NFS/chaincodes/contract-go/main.go
+++ shared_NFS/chaincodes/contract-go/main.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/hyperledger/fabric-chaincode-go/v2/shim"
 	"github.com/hyperledger/fabric-contract-api-go/v2/contractapi"
-	"contract-go/chaincode"
+	"contract-go/pkg"
 )
 
 type serverConfig struct {
@@ -29,7 +29,7 @@ func main() {
 	fmt.Println("[Main] Starting chaincode server...")
 	fmt.Printf("[Main] CHAINCODE_ID: %s, CHAINCODE_SERVER_ADDRESS: %s\n", config.CCID, config.Address)
 
-	chaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	chaincode, err := contractapi.NewChaincode(&pkg.SmartContract{})
 	if err != nil {
 		log.Panicf("Error creating ProductSmartContract chaincode: %v", err)
 	}
