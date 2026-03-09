b0e5bc7a6b9b3875c928bf3b477ef6bac0ee8683
vikash-ftw <vikashbatham97@gmail.com>
2026-03-09 08:31:15 +0000

    fixed issues in chaincode package job

--- shared_NFS/scripts/packageChaincode.sh
+++ shared_NFS/scripts/packageChaincode.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+CHAINCODE_TYPE="${1:-node}"    # default = node
+
 set -e
 
 # -------- CONFIG --------
-CHAINCODE_SRC_DIR="./my-chaincodes/contract-node/packaging"
-# CHAINCODE_SRC_DIR="./my-chaincodes/contract-go/packaging"
+TARGET_DIR="/chaincode/packaging"
 
-TARGET_DIR="./chaincode/packaging"
-
-PACKAGE_NAME="smartcontract-node.tgz"
-# PACKAGE_NAME="smartcontract-go.tgz"
+if [ "$CHAINCODE_TYPE" = "node" ]; then
+  CHAINCODE_PKG_SRC_DIR="/my-chaincodes/contract-node/packaging"
+  PACKAGE_NAME="smartcontract-node.tgz"
+elif [ "$CHAINCODE_TYPE" = "go" ]; then
+  CHAINCODE_PKG_SRC_DIR="/my-chaincodes/contract-go/packaging"
+  PACKAGE_NAME="smartcontract-go.tgz"
+else
+  echo "Invalid chaincode type. use 'node' or 'go'."
+  exit 1
+fi
 
 echo "Starting chaincode packaging..."
 
@@ -20,25 +27,27 @@ if [ ! -d "$TARGET_DIR" ]; then
 fi
 
 # -------- STEP 2: Go to source packaging directory --------
-cd "$CHAINCODE_SRC_DIR"
+cd "$CCHAINCODE_PKG_SRC_DIR"
 
 echo "Current directory: $(pwd)"
 
 # -------- STEP 3: Create code.tar.gz --------
 echo "Creating code.tar.gz..."
-tar -czf code.tar.gz connection.json META-INF
+tar -czf /tmp/code.tar.gz connection.json META-INF
 
 # -------- STEP 4: Create final chaincode package --------
+cp metadata.json /tmp/
+
+echo "$(ls /tmp)"
+
+cd /tmp
 echo "Creating $PACKAGE_NAME..."
 tar -czf "$PACKAGE_NAME" code.tar.gz metadata.json
+echo "$(ls /tmp)"
 
-# -------- STEP 5: Remove temporary file --------
-echo "Cleaning temporary files..."
-rm -f code.tar.gz
-
-# -------- STEP 6: Copy final package --------
+# -------- STEP 5: Copy final package --------
 echo "Copying package to $TARGET_DIR..."
-cp "$PACKAGE_NAME" ../../../chaincode/packaging/
+cp /tmp/"$PACKAGE_NAME" "$TARGET_DIR"
 
 echo "Packaging completed successfully!"
 echo "Chaincode Package available at: $TARGET_DIR/$PACKAGE_NAME"
